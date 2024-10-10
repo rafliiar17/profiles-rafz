812be467a90cbfe9c8df34f842f3152e79edee53
rafz <raflii.arraafi17@gmail.com>
2024-10-10 16:37:50 +0700

    add livechat #1

--- src/App.js
+++ src/App.js
@@ -7,21 +7,22 @@ import Skl from "./skills";
 import { ASCII } from "./tool";
 import { Path, Code, Contact } from "./tool";
 import "./styles/main.css";
+import "./chat";
 
 const paths = ['~\\Home', '~\\Project', '~\\Certificate', '~\\Experience', '~\\Skills', '~\\Project'];
 
 function IntroContent({ index }) {
     return (
-        <div id='app'> 
+        <div id='app'>
             <ASCII />
-            <Path path={paths[index-1]}/>
-            <Code command='About Me'/>
+            <Path path={paths[index - 1]} />
+            <Code command='About Me' />
             <Intro />
-            <Path path={paths[index-1]}/>
-            <Code command='Education'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Education' />
             <Edu />
-            <Path path={paths[index-1]}/>
-            <Code command='Contact Information'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Contact Information' />
             <Contact />
         </div>
     );
@@ -29,13 +30,13 @@ function IntroContent({ index }) {
 
 function ProContent({ index }) {
     return (
-        <div id='app'> 
+        <div id='app'>
             <ASCII />
-            <Path path={paths[index-1]}/>
-            <Code command='Project'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Project' />
             <Pro />
-            <Path path={paths[index-1]}/>
-            <Code command='Contact Information'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Contact Information' />
             <Contact />
         </div>
     );
@@ -43,13 +44,13 @@ function ProContent({ index }) {
 
 function PubContent({ index }) {
     return (
-        <div id='app'> 
+        <div id='app'>
             <ASCII />
-            <Path path={paths[index-1]}/>
-            <Code command='Certificate Owned List'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Certificate Owned List' />
             <Pub />
-            <Path path={paths[index-1]}/>
-            <Code command='Contact Information'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Contact Information' />
             <Contact />
         </div>
     );
@@ -57,13 +58,13 @@ function PubContent({ index }) {
 
 function ExpContent({ index }) {
     return (
-        <div id='app'> 
+        <div id='app'>
             <ASCII />
-            <Path path={paths[index-1]}/>
-            <Code command='Work and Internship'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Work and Internship' />
             <Exp />
-            <Path path={paths[index-1]}/>
-            <Code command='Contact Information'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Contact Information' />
             <Contact />
         </div>
     );
@@ -71,13 +72,13 @@ function ExpContent({ index }) {
 
 function SklContent({ index }) {
     return (
-        <div id='app'> 
+        <div id='app'>
             <ASCII />
-            <Path path={paths[index-1]}/>
-            <Code command='Skills'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Skills' />
             <Skl />
-            <Path path={paths[index-1]}/>
-            <Code command='Contact Information'/>
+            <Path path={paths[index - 1]} />
+            <Code command='Contact Information' />
             <Contact />
         </div>
     );
@@ -86,29 +87,29 @@ function SklContent({ index }) {
 function App(props) {
     // eslint-disable-next-line eqeqeq
     if (props.index == 1) {
-        return <IntroContent index={props.index}/>;
+        return <IntroContent index={props.index} />;
     }
     // eslint-disable-next-line eqeqeq
     else if (props.index == 2) {
-        return <ExpContent index={props.index}/>;
+        return <ExpContent index={props.index} />;
     }
     // eslint-disable-next-line eqeqeq
     else if (props.index == 3) {
-        return <SklContent index={props.index}/>;
+        return <SklContent index={props.index} />;
     }
     // eslint-disable-next-line eqeqeq
     else if (props.index == 4) {
-        return <ProContent index={props.index}/>;
+        return <ProContent index={props.index} />;
     }
     // eslint-disable-next-line eqeqeq
     else if (props.index == 5) {
-        return <PubContent index={props.index}/>;
+        return <PubContent index={props.index} />;
     }
     // TODO : Project
     // eslint-disable-next-line eqeqeq
     else if (props.index == 6) {
-        return <IntroContent index={props.index}/>;
-    }   
+        return <IntroContent index={props.index} />;
+    }
 }
 
 export default App;
